466e586479f702c98d89492169377cc8a74ca656
Michelle Moon <michellemoon236@gmail.com>
2019-05-31 17:18:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,10 +11,7 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    Puppy.new(params[:name], params[:breed], params[:age])
-    @name = params[:name]
-    @breed = params[:breed]
-    @age = params[:age]
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
     
     erb :display_puppy
   end
